284fb3c7b49f66ec466da9eee4fd51fbe489981d
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2025-10-20 09:19:10 +1100

    feat: macos remove rubber band overscroll

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -146,7 +146,7 @@ function SidebarProvider({
             } as React.CSSProperties
           }
           className={cn(
-            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
+            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full fixed top-0 left-0",
             className
           )}
           {...props}
